26706ce7fcb26f37e5639a0a7838551e1367d602
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2023-02-13 22:00:11 -0300

    Slogan created

---
+++ src/components/layout/Slogan.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+  margin-top: 45vh;
+`
+
+const StyledSlogan = styled.p`
+  font-family: 'Ubuntu';
+  color: ${(props) => props.theme.colors.text};
+  width: 400;
+  font-weight: 700;
+  font-size: 38px;
+  line-height: 40px;
+`
+const StyledColor = styled.span`
+  color: #7F1AE3;
+`
+
+export default function Slogan() {
+  return (
+    <Container>
+      <StyledSlogan>
+        Olá, meu nome é <br />
+      </StyledSlogan>
+      <StyledSlogan>
+        {'<'}
+        <StyledColor> Ruan Lopes</StyledColor> <span>{'/>'}</span>
+      </StyledSlogan>
+      <StyledSlogan>Desenvolvedor FullStack</StyledSlogan>
+    </Container>
+  )
+}
