8d9fd446d930a0980fe0e22fab0a6b1f93a9cdac
RuanL-Dev <ruanlopes21@gmail.com>
2023-03-16 21:12:04 -0300

    Hover config for IconImage component

--- src/components/iconImage/IconImage.jsx
+++ src/components/iconImage/IconImage.jsx
@@ -1,6 +1,12 @@
+import styled from 'styled-components'
+
+const StyledIcon = styled.img`
+  cursor: pointer;
+`
+
 export default function IconImage({ imageName, type, size }) {
   // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
-  return <img src={`/icons/${imageName}.${type}`} width={size} />
+  return <StyledIcon src={`/icons/${imageName}.${type}`} width={size} />
 }
 
 IconImage.defaultProps = {
