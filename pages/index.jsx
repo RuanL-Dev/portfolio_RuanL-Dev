2491bafd409c0840e9888755970c7f2b6e8ae47c
RuanL-Dev <ruanlopes21@gmail.com>
2023-03-18 23:51:26 -0300

    CardProject Component Created

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,10 @@ import styled from 'styled-components'
 
 import Navbar from '../src/components/layout/navbar/Navbar'
 import Slogan from '../src/components/layout/Slogan'
-import IconImage from '../src/components/iconImage/IconImage'
+import IconImage from '../src/components/Image/IconImage'
+import ContainerPage from '../src/components/layout/ContainerPage'
 import Button from '../src/components/button/Button'
+import CardProject from '../src/components/card/CardProject'
 
 const StyledImage = styled.div`
   background-image: url('/Debbie_Balboa.gif');
@@ -12,13 +14,9 @@ const StyledImage = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
 `
-const MainContainerAfterGif = styled.div`
-  background: linear-gradient(180.03deg, rgba(32, 37, 48, 0.6) 7.14%, rgba(60, 76, 113, 0.6) 33.17%),
-    #1d2538;
-  width: 100%;
-  min-height: 100vh;
+
+const ContainerDescription = styled.div`
   display: flex;
-  color: ${(props) => props.theme.colors.text};
 `
 const StyledBackgroundImage = styled.div`
   background: #475b74;
@@ -87,6 +85,24 @@ const StyledLineIcons = styled.div`
   gap: 10px;
   margin-top: 20px;
 `
+const StyledContainerProjectsTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 3rem 0;
+`
+const StyledContainerProject = styled.div`
+  display: grid;
+  justify-items: center;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 5px;
+  grid-row-gap: 40px;
+
+  @media (max-width: 900px) {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+  }
+`
 
 export default function HomePage() {
   return (
@@ -95,43 +111,99 @@ export default function HomePage() {
         <Navbar />
         <Slogan />
       </StyledImage>
-      <MainContainerAfterGif>
-        <StyledBackgroundImage>
-          <StyledSelfImage />
-        </StyledBackgroundImage>
-        <StyledContainerDescription>
-          <StyledDescriptionTitle>Sobre mim</StyledDescriptionTitle>
-          <StyledDescriptionSubTitle1>Ruan Lopes Gomes</StyledDescriptionSubTitle1>
-          <StyledDescriptionSubTitle2>Full-Stack Developer</StyledDescriptionSubTitle2>
-          <StyledDescriptionText>
-            Passionate developer looking for a great team to build Apps and WebPages together. I am
-            always looking forward to learning new technologies and overcome new challenges. Happy
-            Happy to relocate for any great opportunity.
-          </StyledDescriptionText>
-          <StyledContainerIcons>
-            <StyledLineIcons>
-              <IconImage imageName="typescript" type="svg" />
-              <IconImage imageName="javascript" type="svg" />
-              <IconImage imageName="html" type="svg" />
-              <IconImage imageName="css" type="svg" />
-            </StyledLineIcons>
-            <StyledLineIcons>
-              <IconImage imageName="react-js" type="svg" />
-              <IconImage imageName="node-js" type="svg" />
-              <IconImage imageName="nextjs" type="svg" />
-            </StyledLineIcons>
-            <StyledLineIcons>
-              <IconImage imageName="github" type="svg" />
-              <IconImage imageName="mongodb" type="svg" />
-            </StyledLineIcons>
-            <StyledLineIcons>
-              <IconImage imageName="StyledComponents" type="svg" />
-            </StyledLineIcons>
-            <Button>Download CV</Button>
-          </StyledContainerIcons>
+      <ContainerPage>
+        <ContainerDescription>
+          <StyledBackgroundImage>
+            <StyledSelfImage />
+          </StyledBackgroundImage>
+          <StyledContainerDescription>
+            <StyledDescriptionTitle>Sobre mim</StyledDescriptionTitle>
+            <StyledDescriptionSubTitle1>Ruan Lopes Gomes</StyledDescriptionSubTitle1>
+            <StyledDescriptionSubTitle2>Full-Stack Developer</StyledDescriptionSubTitle2>
+            <StyledDescriptionText>
+              Passionate developer looking for a great team to build Apps and WebPages together. I
+              am always looking forward to learning new technologies and overcome new challenges.
+              Happy Happy to relocate for any great opportunity.
+            </StyledDescriptionText>
+            <StyledContainerIcons>
+              <StyledLineIcons>
+                <IconImage imageName="typescript" type="svg" />
+                <IconImage imageName="javascript" type="svg" />
+                <IconImage imageName="html" type="svg" />
+                <IconImage imageName="css" type="svg" />
+              </StyledLineIcons>
+              <StyledLineIcons>
+                <IconImage imageName="react-js" type="svg" />
+                <IconImage imageName="node-js" type="svg" />
+                <IconImage imageName="nextjs" type="svg" />
+              </StyledLineIcons>
+              <StyledLineIcons>
+                <IconImage imageName="github" type="svg" />
+                <IconImage imageName="mongodb" type="svg" />
+              </StyledLineIcons>
+              <StyledLineIcons>
+                <IconImage imageName="StyledComponents" type="svg" />
+              </StyledLineIcons>
+              <Button>Download CV</Button>
+            </StyledContainerIcons>
+          </StyledContainerDescription>
+        </ContainerDescription>
+        <StyledContainerProjectsTitle>
           <StyledDescriptionTitle>Projetos</StyledDescriptionTitle>
-        </StyledContainerDescription>
-      </MainContainerAfterGif>
+        </StyledContainerProjectsTitle>
+        <StyledContainerProject>
+          <CardProject
+            image="CbIcon"
+            type="svg"
+            descrição="Desenvolvimento de website para uma organização não governamental sem fins lucrativos que realiza doações a instituições e pessoas 
+            carentes."
+            span="- Uso de tecnologias como ReactJS | NextJs e Styled-components para o desenvolvimento da landing page"
+            title="Challenge do Bem"
+            LinkSite="https://challenge-do-bem-react-js.vercel.app"
+            LinkRepo="https://github.com/AlunosSejaDev/challenge-do-bem-reactJS"
+          />
+          <CardProject
+            image="ClimaDev"
+            type="png"
+            descrição="Site para verificar condições meteorológicas de sua cidade"
+            span="Projeto desenvolvido utilizando ReactJS | NextJs | NodeJs e Styled Components, consumindo a API do Open Weather Map para mostrar 
+            condições meteorológicas atuais e previsão das próximas horas para a cidade selecionada na busca."
+            title="ClimaDev"
+            LinkSite="https://climadev-ruanl-dev.vercel.app"
+            LinkRepo="https://github.com/RuanL-Dev/climadev"
+          />
+          <CardProject
+            image="SocialDevIcon"
+            type="svg"
+            descrição="Rede Social para desenvolvedores"
+            span="Projeto utilizando ReactJs | NextJs | NodeJs e armazenamento de dados do usuário e postagens com banco de dados MongoDB. Outros destaques: Styled-components | 
+            responsividade do site | criação de API para cadastro e login | Token de acesso e JWT | sistema CRUD."
+            title="SocialDev"
+            LinkSite="https://social-dev-ruanl-dev.vercel.app/login"
+            LinkRepo="https://github.com/RuanL-Dev/Social-Dev"
+          />
+          <CardProject
+            image="CrudIcon"
+            type="svg"
+            descrição="Sistema de CRUD para carros"
+            span="Criação de uma API Rest para cadastro, edição e seleção (favoritos) de veículos (CRUD) e possibilidade de deletar o anúncio. Uso de 
+            tecnologias como ReactJs | NextJs | NodeJs | MongoDB e Styled-components"
+            title="CRUD de Veículos"
+            LinkSite="https://ruanl-crud-vehicles.vercel.app"
+            LinkRepo="https://github.com/RuanL-Dev/CRUD_Vehicles"
+          />
+          <CardProject
+            image="JogoDaVelha"
+            type="png"
+            descrição="Projeto desenvolvido com tecnologias ReactJs e NextJs."
+            span="Outros destaques importantes: uso de React Hooks para armazenamento de posições, cliques e definição de campeão da partida 
+            e responsividade do game."
+            title="Jogo da Velha"
+            LinkSite="https://react-jogo-da-velha-ruanl-dev.vercel.app"
+            LinkRepo="https://github.com/RuanL-Dev/react-nextjs-jogo-da-velha"
+          />
+        </StyledContainerProject>
+      </ContainerPage>
     </>
   )
 }
---
+++ src/components/card/CardProject.jsx
@@ -0,0 +1,81 @@
+import styled from 'styled-components'
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const StyledImagen = styled.div`
+  width: ${(props) => props.width}px;
+  height: 250px;
+  border-radius: 5px;
+  background-image: url('/${(props) => props.image}.${(props) => props.type}');
+  background-repeat: no-repeat;
+  background-size: cover;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+
+  :hover {
+    transform: scale(1.1);
+  }
+`
+const CardContainerDescription = styled.div`
+  background-color: ${(props) => props.theme.colors.buttonBackgroundColor};
+  width: 535px;
+  height: 13rem;
+  border-radius: 5px;
+  justify-content: center;
+  text-align: center;
+  padding: 25px 30px 20px 30px;
+`
+const StyledTitle = styled.h3`
+  padding-bottom: 20px;
+  font-size: 25px;
+  line-height: 19px;
+
+  a {
+    color: ${(props) => props.theme.colors.text};
+    font-size: larger;
+    transition: all 0.3s;
+    position: relative;
+    text-decoration: none;
+  }
+  a:after {
+    content: '';
+    position: absolute;
+    background-color: ${(props) => props.theme.colors.text};
+    height: 2px;
+    width: 0;
+    left: 0;
+    bottom: -10px;
+    transition: 0.3s;
+  }
+  a:hover:after {
+    width: 100%;
+  }
+`
+
+function CardProject({ image, type, width, LinkSite, LinkRepo, title, descrição, span }) {
+  return (
+    <CardContainer>
+      <a href={LinkSite} target="_blank" rel="noreferrer">
+        <StyledImagen image={image} type={type} width={width}></StyledImagen>
+      </a>
+      <CardContainerDescription>
+        <StyledTitle>
+          <a href={LinkRepo} target="_blank" rel="noreferrer">
+            {title}
+          </a>
+        </StyledTitle>
+        {descrição}
+        <br />
+        {span}
+      </CardContainerDescription>
+    </CardContainer>
+  )
+}
+
+CardProject.defaultProps = {
+  width: '536px'
+}
+
+export default CardProject
